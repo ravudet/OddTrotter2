8d5cc898770c90dc09ae6643f7ef15edc76db959
ravudet <170215607+ravudet@users.noreply.github.com>
2024-06-18 10:53:18 -0700

    adding unit tests for partitionedmemorycache (#10)

---
+++ OddTrotter.MemoryCachingInteractionLibrary.Tests/Microsoft/Extensions/Caching/Memory/PartitionedMemoryCacheUnitTests.cs
@@ -0,0 +1,159 @@
+﻿namespace Microsoft.Extensions.Caching.Memory
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    /// <summary>
+    /// Unit tests for <see cref="PartitionedMemoryCache{T}"/>
+    /// </summary>
+    [TestClass]
+    public sealed class PartitionedMemoryCacheUnitTests
+    {
+        /// <summary>
+        /// Creates a <see cref="PartitionedMemoryCache{T}"/> with a <see langword="null"/> delegate
+        /// </summary>
+        [TestMethod]
+        public void NullDelegateMemoryCache()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new PartitionedMemoryCache<string>(
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null,
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                string.Empty).Dispose());
+        }
+
+        /// <summary>
+        /// Creates a <see cref="PartitionedMemoryCache{T}"/> with a <see langword="null"/> partition ID
+        /// </summary>
+        [TestMethod]
+        public void NullPartitionId()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new PartitionedMemoryCache<string>(
+                new MemoryCache(new MemoryCacheOptions()),
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                ).Dispose());
+        }
+
+        /// <summary>
+        /// Retrieves a cache entry that doesn't exist
+        /// </summary>
+        [TestMethod]
+        public void RetrieveNonexistentEntry()
+        {
+            using (var memoryCache = new MemoryCache(new MemoryCacheOptions()))
+            {
+                var partitionId = nameof(RetrieveNonexistentEntry);
+                using (var partitionedMemoryCache = new PartitionedMemoryCache<string>(memoryCache, partitionId))
+                {
+                    Assert.IsFalse(partitionedMemoryCache.TryGetValue("test", out var value));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes a cache entry that doesn't exist
+        /// </summary>
+        [TestMethod]
+        public void RemoveNonexistentEntry()
+        {
+            using (var memoryCache = new MemoryCache(new MemoryCacheOptions()))
+            {
+                var partitionId = nameof(RemoveNonexistentEntry);
+                using (var partitionedMemoryCache = new PartitionedMemoryCache<string>(memoryCache, partitionId))
+                {
+                    partitionedMemoryCache.Remove("test");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a cache entry, retrieves it, then removes it
+        /// </summary>
+        [TestMethod]
+        public void CreateRetrieveAndRemove()
+        {
+            using (var memoryCache = new MemoryCache(new MemoryCacheOptions() { ExpirationScanFrequency = TimeSpan.FromDays(1) }))
+            {
+                var partitionId = nameof(RemoveNonexistentEntry);
+                using (var partitionedMemoryCache = new PartitionedMemoryCache<string>(memoryCache, partitionId))
+                {
+                    var entryKey = "test";
+                    Assert.IsFalse(partitionedMemoryCache.TryGetValue(entryKey, out _));
+
+                    partitionedMemoryCache.GetOrCreate(entryKey, entry => "somevalue");
+                    Assert.IsTrue(partitionedMemoryCache.TryGetValue(entryKey, out _));
+
+                    partitionedMemoryCache.Remove(entryKey);
+                    Assert.IsFalse(partitionedMemoryCache.TryGetValue(entryKey, out _));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates two cache entries and retrieves them in a single partition
+        /// </summary>
+        [TestMethod]
+        public void TwoEntriesInOnePartition()
+        {
+            using (var memoryCache = new MemoryCache(new MemoryCacheOptions() { ExpirationScanFrequency = TimeSpan.FromDays(1) }))
+            {
+                var partitionId = nameof(RemoveNonexistentEntry);
+                using (var partitionedMemoryCache = new PartitionedMemoryCache<string>(memoryCache, partitionId))
+                {
+                    var entryKey1 = "test";
+                    var entryKey2 = "test2";
+                    Assert.IsFalse(partitionedMemoryCache.TryGetValue(entryKey1, out _));
+                    Assert.IsFalse(partitionedMemoryCache.TryGetValue(entryKey2, out _));
+
+                    partitionedMemoryCache.GetOrCreate(entryKey1, entry => "somevalue");
+                    Assert.IsTrue(partitionedMemoryCache.TryGetValue(entryKey1, out _));
+                    Assert.IsFalse(partitionedMemoryCache.TryGetValue(entryKey2, out _));
+
+                    partitionedMemoryCache.GetOrCreate(entryKey2, entry => "somevalue");
+                    Assert.IsTrue(partitionedMemoryCache.TryGetValue(entryKey1, out _));
+                    Assert.IsTrue(partitionedMemoryCache.TryGetValue(entryKey2, out _));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates the same cache entry, retrieves it, and removes it in two different partitions
+        /// </summary>
+        [TestMethod]
+        public void CreateRetrieveAndRemoveAcrossPartitions()
+        {
+            using (var memoryCache = new MemoryCache(new MemoryCacheOptions() { ExpirationScanFrequency = TimeSpan.FromDays(1) }))
+            {
+                var partitionId1 = "first";
+                using (var partitionedMemoryCache1 = new PartitionedMemoryCache<string>(memoryCache, partitionId1))
+                {
+                    var partitionId2 = "second";
+                    using (var partitionedMemoryCache2 = new PartitionedMemoryCache<string>(memoryCache, partitionId2))
+                    {
+                        var entryKey = "test";
+                        Assert.IsFalse(partitionedMemoryCache1.TryGetValue(entryKey, out _));
+                        Assert.IsFalse(partitionedMemoryCache2.TryGetValue(entryKey, out _));
+
+                        partitionedMemoryCache1.GetOrCreate(entryKey, entry => "somevalue");
+                        Assert.IsTrue(partitionedMemoryCache1.TryGetValue(entryKey, out _));
+                        Assert.IsFalse(partitionedMemoryCache2.TryGetValue(entryKey, out _));
+
+                        partitionedMemoryCache2.GetOrCreate(entryKey, entry => "somevalue");
+                        Assert.IsTrue(partitionedMemoryCache1.TryGetValue(entryKey, out _));
+                        Assert.IsTrue(partitionedMemoryCache2.TryGetValue(entryKey, out _));
+
+                        partitionedMemoryCache1.Remove(entryKey);
+                        Assert.IsFalse(partitionedMemoryCache1.TryGetValue(entryKey, out _));
+                        Assert.IsTrue(partitionedMemoryCache2.TryGetValue(entryKey, out _));
+
+                        partitionedMemoryCache2.Remove(entryKey);
+                        Assert.IsFalse(partitionedMemoryCache1.TryGetValue(entryKey, out _));
+                        Assert.IsFalse(partitionedMemoryCache2.TryGetValue(entryKey, out _));
+                    }
+                }
+            }
+        }
+    }
+}
--- OddTrotter.MemoryCachingInteractionLibrary/Microsoft/Extensions/Caching/Memory/PartitionedMemoryCache.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/Microsoft/Extensions/Caching/Memory/PartitionedMemoryCache.cs
@@ -30,29 +30,33 @@
             this.partitionId = partitionId;
         }
 
+        /// <inheritdoc/>
         public ICacheEntry CreateEntry(object key)
         {
             var keyContainer = new KeyContainer(this.partitionId, key);
             return this.delegateMemoryCache.CreateEntry(keyContainer);
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
         }
 
+        /// <inheritdoc/>
         public void Remove(object key)
         {
             var keyContainer = new KeyContainer(this.partitionId, key);
             this.delegateMemoryCache.Remove(keyContainer);
         }
 
+        /// <inheritdoc/>
         public bool TryGetValue(object key, out object? value)
         {
             var keyContainer = new KeyContainer(this.partitionId, key);
             return this.delegateMemoryCache.TryGetValue(keyContainer, out value);
         }
 
-        private sealed class KeyContainer
+        private sealed class KeyContainer : IEquatable<KeyContainer>
         {
             public KeyContainer(T partitionId, object key)
             {
@@ -61,7 +65,51 @@
             }
 
             public T PartitionId { get; }
+
             public object Key { get; }
+
+            /// <inheritdoc/>
+            public bool Equals(KeyContainer? other)
+            {
+                if (object.ReferenceEquals(this, other))
+                {
+                    return true;
+                }
+
+                if (other == null)
+                {
+                    return false;
+                }
+
+                return
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                    //// TODO
+                    this.PartitionId.Equals(other.PartitionId) &&
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                    this.Key.Equals(other.Key);
+            }
+
+            /// <inheritdoc/>
+            public override bool Equals(object? obj)
+            {
+                if (obj is KeyContainer keyContainer)
+                {
+                    return this.Equals(keyContainer);
+                }
+
+                return base.Equals(obj);
+            }
+
+            /// <inheritdoc/>
+            public override int GetHashCode()
+            {
+                return
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                    //// TODO
+                    this.PartitionId.GetHashCode() ^
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                    this.Key.GetHashCode();
+            }
         }
     }
 }
