3d99faa33d03f42a4fdce87d74af3f9633cbbedd
ravudet <170215607+ravudet@users.noreply.github.com>
2024-06-21 12:03:39 -0700

    adding unit tests for todolistservicesettings (#13)

---
+++ OddTrotter.MemoryCachingInteractionLibrary.Tests/OddTrotter/TodoList/TodoListServiceSettingsUnitTests.cs
@@ -0,0 +1,83 @@
+﻿namespace OddTrotter.TodoList
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    /// <summary>
+    /// Unit tests for <see cref="TodoListServiceSettings"/>
+    /// </summary>
+    [TestClass]
+    public sealed class TodoListServiceSettingsUnitTests
+    {
+        /// <summary>
+        /// Creates default <see cref="TodoListServiceSettings"/>
+        /// </summary>
+        [TestMethod]
+        public void DefaultSettings()
+        {
+            var builder = new TodoListServiceSettings.Builder();
+            var settings = builder.Build();
+            Assert.AreEqual(builder.CalendarEventPageSize, settings.CalendarEventPageSize);
+            Assert.AreEqual(builder.TodoListDataBlobName, settings.TodoListDataBlobName);
+        }
+
+        /// <summary>
+        /// Creates <see cref="TodoListServiceSettings"/> with a <see langword="null"/> todo list data blob name
+        /// </summary>
+        [TestMethod]
+        public void NullTodoListDataBlobName()
+        {
+            var builder = new TodoListServiceSettings.Builder()
+            {
+                TodoListDataBlobName =
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                    null,
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            };
+
+            Assert.ThrowsException<ArgumentNullException>(() => builder.Build());
+        }
+
+        /// <summary>
+        /// Creates <see cref="TodoListServiceSettings"/> with an empty todo list data blob name
+        /// </summary>
+        [TestMethod]
+        public void EmptyTodoListDataBlobName()
+        {
+            var builder = new TodoListServiceSettings.Builder()
+            {
+                TodoListDataBlobName = string.Empty,
+            };
+
+            Assert.ThrowsException<ArgumentException>(() => builder.Build());
+        }
+
+        /// <summary>
+        /// Creates <see cref="TodoListServiceSettings"/> with a zero page size
+        /// </summary>
+        [TestMethod]
+        public void ZeroPageSize()
+        {
+            var builder = new TodoListServiceSettings.Builder()
+            {
+                CalendarEventPageSize = 0,
+            };
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => builder.Build());
+        }
+
+        /// <summary>
+        /// Creates <see cref="TodoListServiceSettings"/> with a negative page size
+        /// </summary>
+        [TestMethod]
+        public void NegativePageSize()
+        {
+            var builder = new TodoListServiceSettings.Builder()
+            {
+                CalendarEventPageSize = -5,
+            };
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => builder.Build());
+        }
+    }
+}
