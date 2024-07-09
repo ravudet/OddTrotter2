ddfe1d7d1b1d0fcf9898f6e80484fe939432cb5d
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-08 20:53:51 -0700

    adsf

---
+++ OddTrotter.Core/OddTrotter/Calendar/CalendarService.cs
@@ -0,0 +1,27 @@
+﻿namespace OddTrotter.Calendar
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using OddTrotter.GraphClient;
+
+    public sealed class CalendarService
+    {
+        private readonly IGraphClient graphClient;
+
+        public CalendarService(IGraphClient graphClient)
+        {
+            if (graphClient == null)
+            {
+                throw new ArgumentNullException(nameof(graphClient));
+            }
+
+            this.graphClient = graphClient;
+        }
+
+        public async Task<TentativeCalendarResult> RetrieveTentativeCalendar()
+        {
+
+        }
+    }
+}
