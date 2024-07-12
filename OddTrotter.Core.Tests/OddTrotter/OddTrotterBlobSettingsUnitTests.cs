1fda7f2c278ae8e81a40d61a22b4c8cff58a59bb
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-12 08:49:02 -0700

    moved oddtrotterblobsettings to core with new tests (#1)
    * added tests for oddtrotterblobsettings
    
    * moved oddtrotterblobsettings to core

---
+++ OddTrotter.Core.Tests/OddTrotter/OddTrotterBlobSettingsUnitTests.cs
@@ -0,0 +1,59 @@
+﻿namespace OddTrotter
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    /// <summary>
+    /// Unit tests for <see cref="OddTrotterBlobSettings"/>
+    /// </summary>
+    [TestClass]
+    public sealed class OddTrotterBlobSettingsUnitTests
+    {
+        /// <summary>
+        /// Creates default <see cref="OddTrotterBlobSettings"/>
+        /// </summary>
+        [TestMethod]
+        public void DefaultSettings()
+        {
+            var builder = new OddTrotterBlobSettings.Builder()
+            {
+                BlobContainerUrl = "https://a.container",
+                SasToken = "sometoken",
+            };
+            var settings = builder.Build();
+
+            Assert.AreEqual(builder.BlobContainerUrl, settings.BlobContainerUrl);
+            Assert.AreEqual(builder.SasToken, settings.SasToken);
+        }
+
+        /// <summary>
+        /// Creates <see cref="OddTrotterBlobSettings"/> with a <see langword="null"/> blob container URL
+        /// </summary>
+        [TestMethod]
+        public void NullBlobContainerUrl()
+        {
+            var builder = new OddTrotterBlobSettings.Builder()
+            {
+                BlobContainerUrl = null,
+                SasToken = "sometoken",
+            };
+
+            Assert.ThrowsException<ArgumentNullException>(() => builder.Build());
+        }
+
+        /// <summary>
+        /// Creates <see cref="OddTrotterBlobSettings"/> with a <see langword="null"/> blob container URL
+        /// </summary>
+        [TestMethod]
+        public void NullSasToken()
+        {
+            var builder = new OddTrotterBlobSettings.Builder()
+            {
+                BlobContainerUrl = "https://a.container",
+                SasToken = null,
+            };
+
+            Assert.ThrowsException<ArgumentNullException>(() => builder.Build());
+        }
+    }
+}
