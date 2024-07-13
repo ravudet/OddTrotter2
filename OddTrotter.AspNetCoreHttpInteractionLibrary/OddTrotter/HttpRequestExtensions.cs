769d1937bbd41498f865fbe8ccd2ccb8d277f5f2
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-12 20:38:08 -0700

    created an httprequestdata data type in core with an adapter extension method along with unit tests for the data type and the adapter

---
+++ OddTrotter.AspNetCoreHttpInteractionLibrary/OddTrotter/HttpRequestExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace OddTrotter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Http;
+
+    /// <summary>
+    /// Extension methods for <see cref="HttpRequest"/>
+    /// </summary>
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpRequest"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="httpRequest"/> is <see langword="null"/></exception>
+        public static HttpRequestData ToHttpRequestData(this HttpRequest httpRequest)
+        {
+            if (httpRequest == null)
+            {
+                throw new ArgumentNullException(nameof(httpRequest));
+            }
+
+            return new HttpRequestData(httpRequest.Form.ToDictionary(kvp => kvp.Key, kvp => (IReadOnlyList<string>)kvp.Value));
+        }
+    }
+}
