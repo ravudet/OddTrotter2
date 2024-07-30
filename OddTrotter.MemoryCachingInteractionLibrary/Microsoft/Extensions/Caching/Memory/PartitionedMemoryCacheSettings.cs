31b2800f98a28755922c75c0a646cc6f0abb6766
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-30 10:13:49 -0700

    updating partitionedmemorycache to use a comparer to avoid possible null references

---
+++ OddTrotter.MemoryCachingInteractionLibrary/Microsoft/Extensions/Caching/Memory/PartitionedMemoryCacheSettings.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.Extensions.Caching.Memory
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class PartitionedMemoryCacheSettings<T>
+    {
+        private PartitionedMemoryCacheSettings(IEqualityComparer<T> comparer)
+        {
+            this.Comparer = comparer;
+        }
+
+        public static PartitionedMemoryCacheSettings<T> Default { get; } = new PartitionedMemoryCacheSettings<T>(EqualityComparer<T>.Default);
+
+        public IEqualityComparer<T> Comparer { get; }
+
+        public sealed class Builder
+        {
+            public IEqualityComparer<T> Comparer { get; set; } = PartitionedMemoryCacheSettings<T>.Default.Comparer;
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            /// <exception cref="ArgumentNullException">Thrown if <see cref="Comparer"/> is <see langword="null"/></exception>
+            public PartitionedMemoryCacheSettings<T> Build()
+            {
+                if (this.Comparer == null)
+                {
+                    throw new ArgumentNullException(nameof(this.Comparer));
+                }
+
+                return new PartitionedMemoryCacheSettings<T>(this.Comparer);
+            }
+        }
+    }
+}
