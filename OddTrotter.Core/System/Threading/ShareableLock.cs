2055913c819998c0db7ad469a03dceb2b2ce0db1
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-23 09:52:28 -0700

    asdf

--- OddTrotter.Core/System/Threading/ShareableLock.cs
+++ OddTrotter.Core/System/Threading/ShareableLock.cs
@@ -9,21 +9,21 @@ namespace System.Threading
     /// <summary>
     /// TODO check chat from 08/19 to add details
     /// </summary>
-    public sealed class ShareableLock //// TODO better name
+    public sealed class ShareableLock
     {
         private int categoryLock; //// TODO readWriteLock
 
-        private int alignedCount; //// TODO readCount
+        private int sharedLockCount;
 
         public ShareableLock()
         {
             this.categoryLock = 0;
-            this.alignedCount = 1; //// TODO starting at 1 is *very* important
+            this.sharedLockCount = 1; //// TODO starting at 1 is *very* important
         }
 
-        public void EnterReadLock()
+        public void EnterReadLock() //// TODO entershared
         {
-            Interlocked.Increment(ref this.alignedCount);
+            Interlocked.Increment(ref this.sharedLockCount);
             while (true)
             {
                 if (Interlocked.CompareExchange(ref this.categoryLock, 1, 0) == 0)
@@ -44,18 +44,18 @@ namespace System.Threading
 
         public void ExitReadLock()
         {
-            Interlocked.Decrement(ref this.alignedCount);
+            Interlocked.Decrement(ref this.sharedLockCount);
             //// TODO alignedcount might need to be volatile because of this non-reference read...
-            if (this.alignedCount == 1) //// TODO do you need this if statement? don't the below cases still work even if you don't do this check?
+            if (this.sharedLockCount == 1) //// TODO do you need this if statement? don't the below cases still work even if you don't do this check?
             {
                 // if the lock is 0, then another aligned test already released the lock, so we don't need to release it
                 // if the lock is -1, then another aligned test already released the lock and allowed a non-aligned test to acquire it, meaning we don't need to release the lock
                 // if the lock is 1, then we either succeed in releasing because alignedcount has remained 1 (meaning we don't need to re-release), or alignedcount is more than 1 because another aligned test has acquired the lock (meaning that we *shouldn't* release the lock)
-                Interlocked.CompareExchange(ref this.categoryLock, 0, this.alignedCount);
+                Interlocked.CompareExchange(ref this.categoryLock, 0, this.sharedLockCount);
             }
         }
 
-        public void EnterWriteLock()
+        public void EnterWriteLock() //// TODO enterexclusive
         {
             while (true)
             {
