8cbb99d3e8f4d5e1fef89eae9c727d99f9bd9a69
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-11 13:22:14 -0700

    asdf

--- OddTrotter.Core/OddTrotter/Calendar/CalendarService.cs
+++ OddTrotter.Core/OddTrotter/Calendar/CalendarService.cs
@@ -91,157 +91,30 @@
                 .Events
                 .OrderBy(calendarEvent => calendarEvent.Start.DateTime)
                 .Where(calendarEvent => calendarEvent.IsCancelled == false);
-            query.ToArray(); //// TODO is there an async queryable?
+            var queried = query.ToArray(); //// TODO is there an async queryable?
 
-            var instanceEvents = GetInstanceEvents(graphClient, startTime, endTime, pageSize);
-            var seriesEvents = GetSeriesEvents(graphClient, startTime, endTime, pageSize);
-            //// TODO merge the sorted sequences instead of concat
             return new ODataCollection<CalendarEvent>(
-                instanceEvents.Elements.Concat(seriesEvents.Elements),
-                instanceEvents.LastRequestedPageUrl ?? seriesEvents.LastRequestedPageUrl);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="graphClient"></param>
-        /// <param name="startTime"></param>
-        /// <param name="endTime"></param>
-        /// <param name="pageSize"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidAccessTokenException">
-        /// Thrown if the access token configured on <paramref name="graphClient"/> is invalid or provides insufficient privileges for the requests
-        /// </exception>
-        private static ODataCollection<CalendarEvent> GetInstanceEvents(IGraphClient graphClient, DateTime startTime, DateTime endTime, int pageSize)
-        {
-            /*var graphCalendarContext = new GraphCalendarContext(graphClient, new Uri("/me/calendar", UriKind.Relative).ToRelativeUri());
-            var events = graphCalendarContext.Events;
-            events = events
-                .Select(calendarEvent => calendarEvent.Id)
-                .Select(calendarEvent => calendarEvent.Body)
-                .Select(calendarEvent => calendarEvent.Start)
-                .Select(calendarEvent => calendarEvent.Subject)
-                .Select(calendarEvent => calendarEvent.ResponseStatus)
-                .Select(calendarEvent => calendarEvent.WebLink);
-            var values = events
-                .GetValues().GetAwaiter().GetResult(); //// TODO make this async*/
-
-            //// TODO starttime and endtime should be done through a queryable
-            var url = GetInstanceEventsUrl(startTime, pageSize) + $" and start/dateTime lt '{endTime.ToUniversalTime().ToString("yyyy-MM-ddThh:mm:ss.000000")}'";
-            return GetCollection<CalendarEvent>(graphClient, new Uri(url, UriKind.Relative).ToRelativeUri());
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="graphClient"></param>
-        /// <param name="startTime"></param>
-        /// <param name="endTime"></param>
-        /// <param name="pageSize"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidAccessTokenException">
-        /// Thrown if the access token configured on <paramref name="graphClient"/> is invalid or provides insufficient privileges for the requests
-        /// </exception>
-        /// <remarks>
-        /// The returned <see cref="ODataCollection{CalendarEvent}"/> will have its <see cref="ODataCollection{T}.LastRequestedPageUrl"/> be one of three values:
-        /// 1. <see langword="null"/> if no errors occurred retrieve any of the data
-        /// 2. The URL of series master entity for which an error occurred while retrieving the instance events
-        /// 3. The URL of the nextLink for which an error occurred while retrieving the that URL's page
-        /// </remarks>
-        private static ODataCollection<CalendarEvent> GetSeriesEvents(IGraphClient graphClient, DateTime startTime, DateTime endTime, int pageSize)
-        {
-            var seriesEventMasters = GetSeriesEventMasters(graphClient, pageSize);
-            //// TODO you could actually filter by subject here before making further requests
-            var seriesInstanceEvents = seriesEventMasters
-                .Elements
-                .Select(series => (series, GetFirstSeriesInstanceInRange(graphClient, series, startTime, endTime).ConfigureAwait(false).GetAwaiter().GetResult()))
-                .ToV2Enumerable();
-            var seriesInstanceEventsWithFailures = seriesInstanceEvents
-                .ApplyAggregation((string?)null, (failedMasterId, tuple) => tuple.Item2 == null ? tuple.series.Id : null);
-            var seriesInstanceEventsWithoutFailures = seriesInstanceEventsWithFailures
-                .TakeWhile(tuple => tuple.Item2 != null) //// TODO if you can move this before the applyaggregation somehow, that would be ideal since we then wouldn't continue retrieve the first series instance if we already know we aren't returing anymore of them
-                .Where(tuple => tuple.Item2?.Any() == true)
-                .Select(tuple => new CalendarEvent() { Body = tuple.series.Body, Id = tuple.series.Id, Subject = tuple.series.Subject, Start = tuple.Item2?.First().Start });
-
-            // because the second parameter of lastRequestedPageUrl is a fully realized value, we need to have actually enumerated the elements that we expect to return in the
-            // first parameter; in this case, we have enumerated the elements because accessing seriesInstanceEventsWithFailures.Aggregation will enumerate enough events to
-            // perform the aggregation; in a previous iteration of this method, the elements were enumerated with a .ToList() call
-            return new ODataCollection<CalendarEvent>(
-                seriesInstanceEventsWithoutFailures,
-                seriesInstanceEventsWithFailures.Aggregation == null ?
-                    seriesEventMasters.LastRequestedPageUrl :
-                    $"/me/calendar/events/{seriesInstanceEventsWithFailures.Aggregation}");
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="graphClient"></param>
-        /// <param name="pageSize"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidAccessTokenException">
-        /// Thrown if the access token configured on <paramref name="graphClient"/> is invalid or provides insufficient privileges for the requests
-        /// </exception>
-        private static ODataCollection<CalendarEvent> GetSeriesEventMasters(IGraphClient graphClient, int pageSize)
-        {
-            //// TODO make the calendar that's used configurable?
-            var url = $"/me/calendar/events?" +
-                $"$select=body,start,subject,responseStatus,webLink&" +
-                $"$top={pageSize}&" +
-                $"$orderBy=start/dateTime&" +
-                "$filter=type eq 'seriesMaster' and isCancelled eq false";
-            return GetCollection<CalendarEvent>(graphClient, new Uri(url, UriKind.Relative).ToRelativeUri());
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="graphClient"></param>
-        /// <param name="seriesMaster"></param>
-        /// <param name="startTime"></param>
-        /// <param name="endTime"></param>
-        /// <returns>The first instance of the <paramref name="seriesMaster"/> event, or <see langword="null"/> if an error occurred while retrieveing the first instance</returns>
-        /// <exception cref="InvalidAccessTokenException">
-        /// Thrown if the access token configured on <paramref name="graphClient"/> is invalid or provides insufficient privileges for the requests
-        /// </exception>
-        private static async Task<IEnumerable<CalendarEvent>?> GetFirstSeriesInstanceInRange(IGraphClient graphClient, CalendarEvent seriesMaster, DateTime startTime, DateTime endTime)
-        {
-            //// TODO this method would be much better as some sort of "TryGet" variant (though it does still have exceptions that it throws...), but being async, we can't have the needed out parameters
-            var url = $"/me/calendar/events/{seriesMaster.Id}/instances?startDateTime={startTime}&endDateTime={endTime}&$top=1&$select=id,start,subject,body,responseStatus,webLink&$filter=isCancelled eq false";
-            HttpResponseMessage? httpResponse = null;
-            try
-            {
-                try
+                queried.Select(_ => new CalendarEvent()
                 {
-                    httpResponse = await graphClient.GetAsync(new Uri(url, UriKind.Relative).ToRelativeUri()).ConfigureAwait(false);
-                }
-                catch (HttpRequestException)
-                {
-                    return null;
-                }
-
-                if (!httpResponse.IsSuccessStatusCode)
-                {
-                    return null;
-                }
-
-                var httpResponseContent = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-                ODataCollectionPage<CalendarEvent>.Builder? odataCollection;
-                try
-                {
-                    odataCollection = JsonSerializer.Deserialize<ODataCollectionPage<CalendarEvent>.Builder>(httpResponseContent);
-                }
-                catch (JsonException)
-                {
-                    return null;
-                }
-
-                return odataCollection?.Value;
-            }
-            finally
-            {
-                httpResponse?.Dispose();
-            }
+                    Body = new TodoList.BodyStructure()
+                    {
+                        Content = _.Body?.Content
+                    },
+                    Id = _.Id,
+                    ResponseStatus = new TodoList.ResponseStatusStructure()
+                    {
+                        Response = _.ResponseStatus?.Response,
+                        Time = _.ResponseStatus?.Time,
+                    },
+                    Start = new TodoList.TimeStructure()
+                    {
+                        DateTime = _.Start?.DateTime.ToString(),
+                        TimeZone = _.Start?.TimeZone,
+                    },
+                    Subject = _.Subject,
+                    WebLink = _.WebLink,
+                }),
+                null); //// TODO get the last request page uri
         }
 
         /// <summary>
