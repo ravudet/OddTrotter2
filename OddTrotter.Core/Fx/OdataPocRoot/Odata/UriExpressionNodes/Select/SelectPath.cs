e44c8ed0f612a77039e4d4b042cdfdfd361c462e
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-12 09:29:16 -0700

    asdf

--- OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Select/SelectPath.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Select/SelectPath.cs
@@ -1,7 +1,57 @@
 ﻿namespace Fx.OdataPocRoot.Odata.UriExpressionNodes.Select
 {
+    using Fx.OdataPocRoot.Odata.UriExpressionNodes.Common;
+
     public abstract class SelectPath
     {
+        private SelectPath()
+        {
+        }
 
+        public sealed class First : SelectPath
+        {
+            public First(OdataIdentifier complexProperty)
+            {
+                ComplexProperty = complexProperty;
+            }
+
+            public OdataIdentifier ComplexProperty { get; }
+        }
+
+        public sealed class Second : SelectPath
+        {
+            public Second(OdataIdentifier complexProperty, QualifiedComplexTypeName qualifiedComplexTypeName)
+            {
+                ComplexProperty = complexProperty;
+                QualifiedComplexTypeName = qualifiedComplexTypeName;
+            }
+
+            public OdataIdentifier ComplexProperty { get; }
+
+            public QualifiedComplexTypeName QualifiedComplexTypeName { get; }
+        }
+
+        public sealed class Third : SelectPath
+        {
+            public Third(OdataIdentifier complexCollectionProperty)
+            {
+                ComplexCollectionProperty = complexCollectionProperty;
+            }
+
+            public OdataIdentifier ComplexCollectionProperty { get; }
+        }
+
+        public sealed class Fourth : SelectPath
+        {
+            public Fourth(OdataIdentifier complexCollectionProperty, QualifiedComplexTypeName qualifiedComplexTypeName)
+            {
+                ComplexCollectionProperty = complexCollectionProperty;
+                QualifiedComplexTypeName = qualifiedComplexTypeName;
+            }
+
+            public OdataIdentifier ComplexCollectionProperty { get; }
+
+            public QualifiedComplexTypeName QualifiedComplexTypeName { get; }
+        }
     }
 }
