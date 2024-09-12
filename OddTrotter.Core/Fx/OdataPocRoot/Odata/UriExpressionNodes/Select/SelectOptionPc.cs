f718df15095da0270908d2985b3f54ae036a1861
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-12 09:14:25 -0700

    asdf

---
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Select/SelectOptionPc.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Fx.OdataPocRoot.Odata.UriExpressionNodes.Select
+{
+    public abstract class SelectOptionPc
+    {
+        private SelectOptionPc()
+        {
+        }
+
+        public sealed class FilterNode : SelectOptionPc
+        {
+            public FilterNode(Fx.OdataPocRoot.Odata.UriExpressionNodes.Filter.FilterNode filter)
+            {
+                Filter = filter;
+            }
+
+            public UriExpressionNodes.Filter.FilterNode Filter { get; }
+        }
+
+        public sealed class FilterNode : SelectOptionPc
+        {
+            public FilterNode(Fx.OdataPocRoot.Odata.UriExpressionNodes.Filter.FilterNode filter)
+            {
+                Filter = filter;
+            }
+
+            public UriExpressionNodes.Filter.FilterNode Filter { get; }
+        }
+
+        public sealed class FilterNode : SelectOptionPc
+        {
+            public FilterNode(Fx.OdataPocRoot.Odata.UriExpressionNodes.Filter.FilterNode filter)
+            {
+                Filter = filter;
+            }
+
+            public UriExpressionNodes.Filter.FilterNode Filter { get; }
+        }
+
+        public sealed class FilterNode : SelectOptionPc
+        {
+            public FilterNode(Fx.OdataPocRoot.Odata.UriExpressionNodes.Filter.FilterNode filter)
+            {
+                Filter = filter;
+            }
+
+            public UriExpressionNodes.Filter.FilterNode Filter { get; }
+        }
+    }
+}
