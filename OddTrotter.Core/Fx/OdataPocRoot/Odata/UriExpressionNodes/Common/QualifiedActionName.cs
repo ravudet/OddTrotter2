ec0a624db69b3dd4690ef1187c2545cc26b1e059
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-12 09:55:16 -0700

    asdf

--- OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Common/QualifiedActionName.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Common/QualifiedActionName.cs
@@ -2,5 +2,6 @@
 {
     public sealed class QualifiedActionName
     {
+        //// TODO do this
     }
 }
--- OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Common/QualifiedFunctionName.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Common/QualifiedFunctionName.cs
@@ -2,5 +2,6 @@
 {
     public sealed class QualifiedFunctionName
     {
+        //// TODO do this
     }
 }
