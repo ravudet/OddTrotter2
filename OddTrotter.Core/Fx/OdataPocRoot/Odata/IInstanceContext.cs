5367437ff428b71b1afaabd2cb4de954d0e09f94
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-12 14:51:06 -0700

    asdf

--- OddTrotter.Core/Fx/OdataPocRoot/Odata/IInstanceContext.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/IInstanceContext.cs
@@ -7,6 +7,7 @@
     public interface IInstanceContext<T>
     {
         Task<T> Evaluate(); //// TODO use 09/12 whiteboard photo for evaluate method
+        //// TODO do a uri comparer for tests
 
         IInstanceContext<T> Select<TProperty>(Expression<Func<T, TProperty>> selector);
     }
