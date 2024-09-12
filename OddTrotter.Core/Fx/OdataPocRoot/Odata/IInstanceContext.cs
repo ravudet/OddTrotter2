52f040ce43127617f022a387767a9c9766df7a82
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-12 07:04:52 -0700

    asdf

---
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/IInstanceContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Fx.OdataPocRoot.Odata
+{
+    using System.Linq.Expressions;
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IInstanceContext<T>
+    {
+        Task<T> Evaluate();
+
+        IInstanceContext<T> Select<TProperty>(Expression<Func<T, TProperty>> selector);
+    }
+}
