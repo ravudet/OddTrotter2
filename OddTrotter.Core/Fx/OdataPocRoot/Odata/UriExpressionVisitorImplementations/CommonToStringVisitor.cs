23c5389775209b1e7fce8fe328fd8123a215b2ff
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-16 15:17:07 -0700

    asdf

---
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionVisitorImplementations/CommonToStringVisitor.cs
@@ -0,0 +1,105 @@
+﻿namespace Fx.OdataPocRoot.Odata.UriExpressionVisitorImplementations
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Fx.OdataPocRoot.Odata.UriExpressionNodes.Common;
+
+    public sealed class CommonToStringVisitor
+    {
+        private CommonToStringVisitor()
+        {
+        }
+
+        public static CommonToStringVisitor Default { get; } = new CommonToStringVisitor();
+
+        public void Visit(Namespace node, StringBuilder builder)
+        {
+            //// TODO should the node have a readonlylist of namespace parts?
+            var namespaceParts = node.NamespaceParts.ToList();
+            if (namespaceParts.Count == 0)
+            {
+                //// TODO is this actually legal? model it somehow if it's not
+                return;
+            }
+
+            Visit(namespaceParts[0], builder);
+            for (int i = 1; i < namespaceParts.Count; ++i)
+            {
+                builder.Append(".");
+                Visit(namespaceParts[i], builder);
+            }
+        }
+
+        public void Visit(OdataIdentifier node, StringBuilder builder)
+        {
+            builder.Append(node.Identifier); //// TODO is there really nothign els eyou need?
+        }
+
+        public void Visit(QualifiedEntityTypeName node, StringBuilder builder)
+        {
+            Visit(node.Namespace, builder);
+            builder.Append(".");
+            Visit(node.EntityTypeName, builder);
+        }
+
+        public void Visit(PrimitiveProperty node, StringBuilder builder)
+        {
+            if (node is PrimitiveProperty.PrimitiveKeyProperty primitiveKeyProperty)
+            {
+                Visit(primitiveKeyProperty.Identifier, builder);
+            }
+            else if (node is PrimitiveProperty.PrimitiveNonKeyProperty primitiveNonKeyProperty)
+            {
+                Visit(primitiveNonKeyProperty.Identifier, builder);
+            }
+            else
+            {
+                throw new Exception("TODO a proper visitor pattern would prevent this branch");
+            }
+        }
+
+        public void Visit(NavigationProperty node, StringBuilder builder)
+        {
+            if (node is NavigationProperty.EntityNavigationProperty entityNavigationProperty)
+            {
+                Visit(entityNavigationProperty.Identifier, builder);
+            }
+            else if (node is NavigationProperty.EntityCollectionNavigationProperty entityCollectionNavigationProperty)
+            {
+                Visit(entityCollectionNavigationProperty.Identifier, builder);
+            }
+            else
+            {
+                throw new Exception("TODO a proper visitor pattern would prevent this branch");
+            }
+        }
+
+        public void Visit(QualifiedComplexTypeName node, StringBuilder builder)
+        {
+            Visit(node.Namespace, builder);
+            builder.Append(".");
+            Visit(node.EntityTypeName, builder);
+        }
+
+        public void Visit(AliasAndValue node, StringBuilder builder)
+        {
+            throw new Exception("tODO aliasandvalue is not supported yet");
+        }
+
+        public void Visit(QualifiedActionName node, StringBuilder builder)
+        {
+            Visit(node.Namespace, builder);
+            builder.Append(".");
+            Visit(node.Action, builder);
+        }
+
+        public void Visit(QualifiedFunctionName node, StringBuilder builder)
+        {
+            Visit(node.Namespace, builder);
+            builder.Append(".");
+            Visit(node.Function, builder);
+        }
+    }
+}
--- OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionVisitorImplementations/SelectToStringVisitor.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionVisitorImplementations/SelectToStringVisitor.cs
@@ -11,7 +11,7 @@
 
     public sealed class SelectToStringVisitor
     {
-        private readonly CommonToStringVisitor commonToStringVisitor = CommonToStringVisitor.Default; //// TODO factor this to a common place and use constructor injection
+        private readonly CommonToStringVisitor commonToStringVisitor;
 
         private readonly FilterToStringVisitor filterToStringVisitor;
 
@@ -33,6 +33,7 @@
 
         public SelectToStringVisitor()
             : this(
+                  CommonToStringVisitor.Default,
                   new FilterToStringVisitor(),
                   new SearchToStringVisitor(),
                   new InlineCountToStringVisitor(),
@@ -45,8 +46,9 @@
         {
         }
 
-        public SelectToStringVisitor(FilterToStringVisitor filterToStringVisitor, SearchToStringVisitor searchToStringVisitor, InlineCountToStringVisitor inlineCountToStringVisitor, OrderByToStringVisitor orderByToStringVisitor, SkipToStringVisitor skipToStringVisitor, TopToStringVisitor topToStringVisitor, ComputeToStringVisitor computeToStringVisitor, ExpandToStringVisitor expandToStringVisitor, AliasAndValueToStringVisitor aliasAndValueToStringVisitor)
+        public SelectToStringVisitor(CommonToStringVisitor commonToStringVisitor, FilterToStringVisitor filterToStringVisitor, SearchToStringVisitor searchToStringVisitor, InlineCountToStringVisitor inlineCountToStringVisitor, OrderByToStringVisitor orderByToStringVisitor, SkipToStringVisitor skipToStringVisitor, TopToStringVisitor topToStringVisitor, ComputeToStringVisitor computeToStringVisitor, ExpandToStringVisitor expandToStringVisitor, AliasAndValueToStringVisitor aliasAndValueToStringVisitor)
         {
+            this.commonToStringVisitor = commonToStringVisitor;
             this.filterToStringVisitor = filterToStringVisitor;
             this.searchToStringVisitor = searchToStringVisitor;
             this.inlineCountToStringVisitor = inlineCountToStringVisitor;
@@ -303,103 +305,6 @@
             else
             {
                 throw new Exception("TODO a proper visitor pattern would prevent this branch");
-            }
-        }
-
-        private sealed class CommonToStringVisitor
-        {
-            private CommonToStringVisitor()
-            {
-            }
-
-            public static CommonToStringVisitor Default { get; } = new CommonToStringVisitor();
-
-            public void Visit(Namespace node, StringBuilder builder)
-            {
-                //// TODO should the node have a readonlylist of namespace parts?
-                var namespaceParts = node.NamespaceParts.ToList();
-                if (namespaceParts.Count == 0)
-                {
-                    //// TODO is this actually legal? model it somehow if it's not
-                    return;
-                }
-
-                Visit(namespaceParts[0], builder);
-                for (int i = 1; i < namespaceParts.Count; ++i)
-                {
-                    builder.Append(".");
-                    Visit(namespaceParts[i], builder);
-                }
-            }
-
-            public void Visit(OdataIdentifier node, StringBuilder builder)
-            {
-                builder.Append(node.Identifier); //// TODO is there really nothign els eyou need?
-            }
-
-            public void Visit(QualifiedEntityTypeName node, StringBuilder builder)
-            {
-                Visit(node.Namespace, builder);
-                builder.Append(".");
-                Visit(node.EntityTypeName, builder);
-            }
-
-            public void Visit(PrimitiveProperty node, StringBuilder builder)
-            {
-                if (node is PrimitiveProperty.PrimitiveKeyProperty primitiveKeyProperty)
-                {
-                    Visit(primitiveKeyProperty.Identifier, builder);
-                }
-                else if (node is PrimitiveProperty.PrimitiveNonKeyProperty primitiveNonKeyProperty)
-                {
-                    Visit(primitiveNonKeyProperty.Identifier, builder);
-                }
-                else
-                {
-                    throw new Exception("TODO a proper visitor pattern would prevent this branch");
-                }
-            }
-
-            public void Visit(NavigationProperty node, StringBuilder builder)
-            {
-                if (node is NavigationProperty.EntityNavigationProperty entityNavigationProperty)
-                {
-                    Visit(entityNavigationProperty.Identifier, builder);
-                }
-                else if (node is NavigationProperty.EntityCollectionNavigationProperty entityCollectionNavigationProperty)
-                {
-                    Visit(entityCollectionNavigationProperty.Identifier, builder);
-                }
-                else
-                {
-                    throw new Exception("TODO a proper visitor pattern would prevent this branch");
-                }
-            }
-
-            public void Visit(QualifiedComplexTypeName node, StringBuilder builder)
-            {
-                Visit(node.Namespace, builder);
-                builder.Append(".");
-                Visit(node.EntityTypeName, builder);
-            }
-
-            public void Visit(AliasAndValue node, StringBuilder builder)
-            {
-                throw new Exception("tODO aliasandvalue is not supported yet");
-            }
-
-            public void Visit(QualifiedActionName node, StringBuilder builder)
-            {
-                Visit(node.Namespace, builder);
-                builder.Append(".");
-                Visit(node.Action, builder);
-            }
-
-            public void Visit(QualifiedFunctionName node, StringBuilder builder)
-            {
-                Visit(node.Namespace, builder);
-                builder.Append(".");
-                Visit(node.Function, builder);
             }
         }
     }
