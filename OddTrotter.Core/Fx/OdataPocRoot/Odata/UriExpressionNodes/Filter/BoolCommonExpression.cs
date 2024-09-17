5c09a3920d6ed73dafe3475d095ee76c72b7347e
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-16 19:35:09 -0700

    asdf

--- OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Filter/BoolCommonExpression.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/UriExpressionNodes/Filter/BoolCommonExpression.cs
@@ -2,6 +2,7 @@
 {
     using Fx.OdataPocRoot.Odata.UriExpressionNodes.Common;
 
+    //// TODO finish the rest of boolCommonExpr.abnf
     public abstract class BoolCommonExpression
     {
         private BoolCommonExpression()
@@ -19,98 +20,5 @@
         }
 
         //// TODO do other derived types after you've created abnf for boolcommonexpr
-        /*
-
-
-
-
-boolCommonExpr =
-                (
-                boolPrimitiveLiteral
-                / boolRootExpr
-                / boolFirstMemberExpr
-                / boolFunctionExpr
-                / boolMethodCallExpr
-                / boolParenExpr
-                / boolCastExpr
-                / isofExpr
-                / notExpr
-                / primitiveLiteral eqExpr
-                / primitiveLiteral neExpr
-                / primitiveLiteral ltExpr
-                / primitiveLiteral leExpr
-                / primitiveLiteral gtExpr
-                / primitiveLiteral geExpr
-                / primitiveLiteral hasExpr
-                / primitiveLiteral inExpr
-                / TODO
-                / boolPrimitiveLiteral andExpr
-                / boolPrimitiveLiteral orExpr
-                / TODO
-                )
-
-boolCastExpr =
-                TODO only if you can cast to bool
-
-boolParenExpr =
-                OPEN BWS boolCommonExpr BWS CLOSE
-
-boolFunctionExpr = 
-                    TODO
-
-boolPrimitiveLiteral =
-                        booleanValue
-
-boolRootExpr =
-                TODO
-
-boolFirstMemberExpr =
-                        boolMemberExpr
-                        / boolInscopeVariableExpr [ "/" boolMemberExpr ]
-
-boolMemberExpr = 
-                [ qualifiedEntityTypeName "/" ]
-                (
-                boolPropertyPathExpr
-                / boundFunctionExpr
-                / boolAnnotationExpr
-                )
-
-boolPropertyPathExpr = 
-                        (
-                        entityColNavigationProperty boolCollectionNavigationExpr
-                        / entityNavigationProperty  boolSingleNavigationExpr
-                        / complexColProperty boolComplexColPathExpr
-                        / primitiveColProperty boolCollectionPathExpr
-                        / primitiveProperty [ boolPrimitivePathExpr ]
-                        / streamProperty boolPrimitivePathExpr
-                        )
-
-boolPrimitivePathExpr =
-                        "/" [ boolAnnotationExpr / boundFunctionExpr ]
-
-boolComplexColPathExpr = 
-                        [ "/" qualifiedComplexTypeName ]
-                        [ boolCollectionPathExpr ]
-
-boolCollectionNavigationExpr = 
-                                [ "/" qualifiedEntityTypeName ]
-                                [ keyPredicate boolSingleNavigationExpr
-                                / boolCollectionPathExpr
-                                ]
-
-boolSingleNavigationExpr = 
-                            "/" boolMemberExpr
-
-boolCollectionPathExpr = 
-                        TODO
-
-boolAnnotationExpr =
-                    TODO
-
-boolInscopeVariableExpr =
-                            implicitVariableExpr
-                            / parameterAlias
-        */
     }
 }
