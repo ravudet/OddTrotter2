dcc14d7977854a3b97cd0d163873b6ea4a447c54
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-19 07:37:29 -0700

    asdf

---
+++ OddTrotter.Core/System/RelativeUriComponents.cs
@@ -0,0 +1,14 @@
+﻿namespace System
+{
+    /// <summary>
+    /// The individual parts of a relative URI, corresponding to the absolute URI parts defined by <see cref="UriComponents"/>
+    /// </summary>
+    [Flags]
+    public enum RelativeUriComponents
+    {
+        Path = 1 << 0,
+        Query = 1 << 1,
+        Fragment = 1 << 2,
+        KeepDelimiter = (int.MaxValue >> 1) + 1, // this is the value of System.UriComponents.KeepDelimiter
+    }
+}
