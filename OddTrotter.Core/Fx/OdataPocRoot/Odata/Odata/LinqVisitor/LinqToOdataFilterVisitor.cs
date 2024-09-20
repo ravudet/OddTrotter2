69935f5a2baa24028e0d8885a1f11f40c2da9a79
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-20 07:31:33 -0700

    asdf

--- OddTrotter.Core/Fx/OdataPocRoot/Odata/Odata/LinqVisitor/LinqToOdataFilterVisitor.cs
+++ OddTrotter.Core/Fx/OdataPocRoot/Odata/Odata/LinqVisitor/LinqToOdataFilterVisitor.cs
@@ -19,13 +19,151 @@ namespace Fx.OdataPocRoot.Odata.Odata.LinqVisitor
         }
     }
 
+    public sealed class LinqToBoolCommonExpressionVisitor : ExpressionVisitor<BoolCommonExpression, Void>
+    {
+        protected override BoolCommonExpression Visit(BinaryExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(BlockExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(ConditionalExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(ConstantExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(DebugInfoExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(DefaultExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(DynamicExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(GotoExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(IndexExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(InvocationExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(LabelExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(LambdaExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(ListInitExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(LoopExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(MemberExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(MemberInitExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(MethodCallExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(NewArrayExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(NewExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(ParameterExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(RuntimeVariablesExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(SwitchExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(TryExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(TypeBinaryExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BoolCommonExpression Visit(UnaryExpression node, Void context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public sealed class LinqToOdataFilterVisitor : ExpressionVisitor<Filter, Void>
     {
+        private readonly LinqToBoolCommonExpressionVisitor linqToBoolCommonExpressionVisitor;
+
+        public LinqToOdataFilterVisitor(
+            LinqToBoolCommonExpressionVisitor linqToBoolCommonExpressionVisitor)
+        {
+            //// TODO does constructor injection make sense here? or is context of the use-case too important?
+            this.linqToBoolCommonExpressionVisitor = linqToBoolCommonExpressionVisitor;
+        }
+
         protected override Filter Visit(BinaryExpression node, Void context)
         {
             //// TODO does it make sense to treat this as "this method converts a 'binaryexpression' to a 'filter'" and then have another visitor for "this method converts a 'binaryexpression' to a 'boolcommonexpression'" and another visitor for "this method converts a 'binaryexpression' to a 'boolmethodcallexpression'" and so on?
             if (node.Type != typeof(bool))
             {
+                //// TODO despite having a lot of inforation, this message doesn't actually comunicate that it's the return type of "bool" that's the problem
                 throw new System.Exception($"It is not meaningful to convert the following to an OData 'Filter' expression: a .NET '{node.GetType().Name}' with method '{node.Method?.Name}' and node type '{node.NodeType}' that returns '{node.Type.Name}'");
             }
 
