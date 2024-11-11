bb4616ef4185215890c26e368aec6d1e108fdce3
ravudet <170215607+ravudet@users.noreply.github.com>
2024-11-11 11:00:22 -0800

    asdf

--- OddTrotter.Core/OddTrotter/Calendar/IQueryContext.cs
+++ OddTrotter.Core/OddTrotter/Calendar/IQueryContext.cs
@@ -7,91 +7,6 @@ namespace OddTrotter.Calendar
     using System.Threading.Tasks;
     using static System.Net.Mime.MediaTypeNames;
 
-    public static class Either
-    {
-        public static Either<TLeft, TRight>.Visitor<TResult, TContext> Visitor<TLeft, TRight, TResult, TContext>(
-            Func<Either<TLeft, TRight>.Left, TContext, TResult> leftDispatch,
-            Func<Either<TLeft, TRight>.Right, TContext, TResult> rightDispatch)
-        {
-            return new DelelgateVisitor<TLeft, TRight, TResult, TContext>(leftDispatch, rightDispatch);
-        }
-
-        private sealed class DelelgateVisitor<TLeft, TRight, TResult, TContext> : Either<TLeft, TRight>.Visitor<TResult, TContext>
-        {
-            private readonly Func<Either<TLeft, TRight>.Left, TContext, TResult> leftDispatch;
-            private readonly Func<Either<TLeft, TRight>.Right, TContext, TResult> rightDispatch;
-
-            public DelelgateVisitor(
-                Func<Either<TLeft, TRight>.Left, TContext, TResult> leftDispatch,
-                Func<Either<TLeft, TRight>.Right, TContext, TResult> rightDispatch)
-            {
-                this.leftDispatch = leftDispatch;
-                this.rightDispatch = rightDispatch;
-            }
-
-            public override TResult Dispatch(Either<TLeft, TRight>.Left node, TContext context)
-            {
-                return this.leftDispatch(node, context);
-            }
-
-            public override TResult Dispatch(Either<TLeft, TRight>.Right node, TContext context)
-            {
-                return this.rightDispatch(node, context);
-            }
-        }
-    }
-
-    public abstract class Either<TLeft, TRight>
-    {
-        private Either()
-        {
-        }
-
-        protected abstract TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context);
-
-        public abstract class Visitor<TResult, TContext>
-        {
-            public TResult Visit(Either<TLeft, TRight> node, TContext context)
-            {
-                return node.Accept(this, context);
-            }
-
-            public abstract TResult Dispatch(Left node, TContext context);
-
-            public abstract TResult Dispatch(Right node, TContext context);
-        }
-
-        public sealed class Left : Either<TLeft, TRight>
-        {
-            public Left(TLeft value)
-            {
-                Value = value;
-            }
-
-            public TLeft Value { get; }
-
-            protected override TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
-            {
-                return visitor.Dispatch(this, context);
-            }
-        }
-
-        public sealed class Right : Either<TLeft, TRight>
-        {
-            public Right(TRight value)
-            {
-                Value = value;
-            }
-
-            public TRight Value { get; }
-
-            protected override TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
-            {
-                return visitor.Dispatch(this, context);
-            }
-        }
-    }
-
     public interface IQueryContext<TValue, TError>
     {
         Task<QueryResult<TValue, TError>> Evaluate();
@@ -103,10 +18,31 @@ namespace OddTrotter.Calendar
         {
         }
 
+        protected abstract TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context);
+
+        public abstract class Visitor<TResult, TContext>
+        {
+            public TResult Visit(QueryResult<TValue, TError> node, TContext context)
+            {
+                return node.Accept(this, context);
+            }
+
+            public abstract TResult Dispatch(Final node, TContext context);
+
+            public abstract TResult Dispatch(Element node, TContext context);
+
+            public abstract TResult Dispatch(Partial node, TContext context);
+        }
+
         public sealed class Final : QueryResult<TValue, TError>
         {
             public Final()
             {
+            }
+
+            protected override TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Dispatch(this, context);
             }
         }
 
@@ -121,6 +57,11 @@ namespace OddTrotter.Calendar
             public TValue Value { get; }
 
             public Func<QueryResult<TValue, TError>> Next { get; } //// TODO how inefficient is it to use Task here when the element is already known?
+
+            protected override TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Dispatch(this, context);
+            }
         }
 
         public sealed class Partial : QueryResult<TValue, TError>
@@ -131,6 +72,11 @@ namespace OddTrotter.Calendar
             }
 
             public TError Error { get; }
+
+            protected override TResult Accept<TResult, TContext>(Visitor<TResult, TContext> visitor, TContext context)
+            {
+                return visitor.Dispatch(this, context);
+            }
         }
     }
 
