aad9b019176590b2520891c4adbf9fe6eb2d3c00
ravudet <170215607+ravudet@users.noreply.github.com>
2025-01-03 08:10:21 -0800

    asdf

--- OddTrotter.Core/OddTrotter/Calendar/IGraphCalendarContext.cs
+++ OddTrotter.Core/OddTrotter/Calendar/IGraphCalendarContext.cs
@@ -352,6 +352,11 @@
             }
 
             /// <inheritdoc/>
+            /// <exception cref="HttpRequestException">Thrown if the request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout, or the server responded with a payload that was not valid HTTP</exception> //// TODO this part about the invalid HTTP needs to be added to all of your xmldoc where you get an httprequestexception from httpclient
+            /// <exception cref="OdataErrorDeserializationException">Thrown if an error occurred while deserializing the OData error response</exception>
+            /// <exception cref="OdataSuccessDeserializationException">Thrown if an error occurred while deserializing the OData success response</exception>
+            /// <exception cref="UnauthorizedAccessTokenException">Thrown if the access token used is invalid or provides insufficient privileges for the request</exception>
+            /// <exception cref="GraphProcessingException">Thrown if graph encountered an error processing the request</exception>
             public sealed override async Task<GraphCalendarEventsResponse> DispatchAsync(GraphQuery.Page node, Void context)
             {
                 if (node == null)
@@ -359,14 +364,17 @@
                     throw new ArgumentNullException(nameof(node));
                 }
 
-                //// TODO you are here
                 return await this.GetPage(node.RelativeUri).ConfigureAwait(false);
             }
 
             /// <inheritdoc/>
+            /// <exception cref="HttpRequestException">Thrown if the request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout, or the server responded with a payload that was not valid HTTP</exception> //// TODO this part about the invalid HTTP needs to be added to all of your xmldoc where you get an httprequestexception from httpclient
+            /// <exception cref="OdataErrorDeserializationException">Thrown if an error occurred while deserializing the OData error response</exception>
+            /// <exception cref="OdataSuccessDeserializationException">Thrown if an error occurred while deserializing the OData success response</exception>
+            /// <exception cref="UnauthorizedAccessTokenException">Thrown if the access token used is invalid or provides insufficient privileges for the request</exception>
+            /// <exception cref="GraphProcessingException">Thrown if graph encountered an error processing the request</exception>
             internal sealed override async Task<GraphCalendarEventsResponse> DispatchAsync(GraphQuery.GetEvents node, Void context)
             {
-                //// TODO you are here
                 if (node == null)
                 {
                     throw new ArgumentNullException(nameof(node));
