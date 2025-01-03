ec76fdf01b98f2ea621678bd67914bd305d71f09
ravudet <170215607+ravudet@users.noreply.github.com>
2025-01-03 07:55:30 -0800

    asdf

--- OddTrotter.Core/OddTrotter/Calendar/GraphOdataStructuredContext.cs
+++ OddTrotter.Core/OddTrotter/Calendar/GraphOdataStructuredContext.cs
@@ -15,7 +15,11 @@
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        /// TODO you are here
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="request"/> is <see langword="null"/></exception>
+        /// <exception cref="HttpRequestException">Thrown if the request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout, or the server responded with a payload that was not valid HTTP</exception> //// TODO this part about the invalid HTTP needs to be added to all of your xmldoc where you get an httprequestexception from httpclient
+        /// <exception cref="OdataErrorDeserializationException">Thrown if an error occurred while deserializing the OData error response</exception>
+        /// <exception cref="OdataSuccessDeserializationException">Thrown if an error occurred while deserializing the OData success response</exception>
+        /// <exception cref="UnauthorizedAccessTokenException">Thrown if the access token used is invalid or provides insufficient privileges for the request</exception>
         Task<OdataResponse<OdataCollectionResponse>> GetCollection(OdataGetCollectionRequest request);
     }
 
@@ -94,16 +98,7 @@
 
         public OdataServiceRoot ServiceRoot { get; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="request"/> is <see langword="null"/></exception>
-        /// <exception cref="HttpRequestException">Thrown if the request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout, or the server responded with a payload that was not valid HTTP</exception> //// TODO this part about the invalid HTTP needs to be added to all of your xmldoc where you get an httprequestexception from httpclient
-        /// <exception cref="OdataErrorDeserializationException">Thrown if an error occurred while deserializing the OData error response</exception>
-        /// <exception cref="OdataSuccessDeserializationException">Thrown if an error occurred while deserializing the OData success response</exception>
-        /// <exception cref="UnauthorizedAccessTokenException">Thrown if the access token used is invalid or provides insufficient privileges for the request</exception>
+        /// <inheritdoc/>
         public async Task<OdataResponse<OdataCollectionResponse>> GetCollection(OdataGetCollectionRequest request)
         {
             if (request == null)
@@ -122,13 +117,12 @@
 
             if (response.HttpStatusCode == System.Net.HttpStatusCode.Unauthorized || response.HttpStatusCode == System.Net.HttpStatusCode.Forbidden)
             {
-                //// TODO you are here
                 throw new GraphClient.UnauthorizedAccessTokenException(
                     request.RelativeUri.OriginalString,
                     this.accessToken,
                     response.ResponseContent.Visit(
-                        left => JsonSerializer.Serialize(left),
-                        right => JsonSerializer.Serialize(right)));
+                        left => JsonSerializer.Serialize(left), //// TODO since we were able to deserialize, i *think* we can trust that this won't throw
+                        right => JsonSerializer.Serialize(right))); //// TODO since we were able to deserialize, i *think* we can trust that this won't throw
             }
 
             return response;
