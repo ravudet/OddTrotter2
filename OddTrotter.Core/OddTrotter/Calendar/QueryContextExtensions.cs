d39ad3837f1d707fe066813c0c8337c6ceea8c3d
ravudet <170215607+ravudet@users.noreply.github.com>
2025-01-25 13:37:56 -0800

    asdf

--- OddTrotter.Core/OddTrotter/Calendar/QueryContextExtensions.cs
+++ OddTrotter.Core/OddTrotter/Calendar/QueryContextExtensions.cs
@@ -20,6 +20,6 @@
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="predicate"/> is <see langword="null"/></exception>
         /// <exception cref="NotSupportedException">Thrown if <paramref name="predicate"/> is not supported by the underlying data store</exception>
         /// <exception cref="NotImplementedException">Thrown if support for <paramref name="predicate"/> is not yet implement; this *could* mean that <paramref name="predicate"/> is not supported by the underyling data store and such a check is not yet implemented, or it could mean that <paramref name="predicate"/> *is* supported by the underlying data store, but converting it to the proper query hasn't been implemented yet</exception>
-        TQueryContext Where(Expression<Func<TValue, bool>> predicate);
+        TQueryContext Where(Expression<Func<TValue, bool>> predicate); //// TODO this needs to have a different name; there needs to be an extension method with the name `where` that calls interface and handles monads where available
     }
 }
