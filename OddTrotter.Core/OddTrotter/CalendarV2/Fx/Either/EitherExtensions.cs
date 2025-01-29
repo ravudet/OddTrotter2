18c01001811671f85e01f91dd587a3e2d51c455b
ravudet <170215607+ravudet@users.noreply.github.com>
2025-01-29 10:47:14 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Either/EitherExtensions.cs
@@ -21,7 +21,7 @@
         /// <exception cref="Exception">
         /// Throws any of the exceptions that <paramref name="leftAccept"/> or <paramref name="rightAccept"/> can throw
         /// </exception> //// TODO TOPIC wrap this exception maybe?
-        public static TResult Visit<TLeft, TRight, TResult>(
+        public static TResult Visit<TLeft, TRight, TResult>( //// TODO TOPIC call this "aggregate" instead?
             this IEither<TLeft, TRight> either, 
             Func<TLeft, TResult> leftAccept,
             Func<TRight, TResult> rightAccept)
@@ -56,7 +56,7 @@
         /// <exception cref="Exception">
         /// Throws any of the exceptions that <paramref name="leftSelector"/> or <paramref name="rightSelector"/> can throw
         /// </exception> //// TODO TOPIC wrap this exception maybe?
-        public static IEither<TLeftResult, TRightResult> Select
+        public static IEither<TLeftResult, TRightResult> Select //// TODO TOPIC name this "map" instead?
             <
                 TLeftValue, 
                 TRightValue, 
@@ -77,6 +77,86 @@
             return either.Visit(
                 (left, context) => Either.Left(leftSelector(left, context)).Right<TRightResult>(),
                 (right, context) => Either.Left<TLeftResult>().Right(rightSelector(right, context)),
+                context);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TLeftValue"></typeparam>
+        /// <typeparam name="TRightValue"></typeparam>
+        /// <typeparam name="TLeftResult"></typeparam>
+        /// <typeparam name="TRightResult"></typeparam>
+        /// <typeparam name="TContext"></typeparam>
+        /// <param name="either"></param>
+        /// <param name="leftSelector"></param>
+        /// <param name="rightSelector"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="either"/> or <paramref name="leftSelector"/> is <see langword="null"/>
+        /// </exception>
+        /// <exception cref="Exception">
+        /// Throws any of the exceptions that <paramref name="leftSelector"/> can throw
+        /// </exception> //// TODO TOPIC wrap this exception maybe?
+        public static IEither<TLeftResult, TRightValue> SelectLeft //// TODO TOPIC name this "map" instead? //// TODO TOPIC what about the overall name?
+            <
+                TLeftValue,
+                TRightValue,
+                TLeftResult,
+                TContext
+            >
+            (
+                this IEither<TLeftValue, TRightValue> either,
+                Func<TLeftValue, TContext, TLeftResult> leftSelector,
+                TContext context)
+        {
+            ArgumentNullException.ThrowIfNull(either);
+            ArgumentNullException.ThrowIfNull(leftSelector);
+
+            return either.Visit(
+                (left, context) => Either.Left(leftSelector(left, context)).Right<TRightValue>(),
+                (right, context) => Either.Left<TLeftResult>().Right(right),
+                context);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TLeftValue"></typeparam>
+        /// <typeparam name="TRightValue"></typeparam>
+        /// <typeparam name="TLeftResult"></typeparam>
+        /// <typeparam name="TRightResult"></typeparam>
+        /// <typeparam name="TContext"></typeparam>
+        /// <param name="either"></param>
+        /// <param name="leftSelector"></param>
+        /// <param name="rightSelector"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="either"/> or <paramref name="rightSelector"/> is <see langword="null"/>
+        /// </exception>
+        /// <exception cref="Exception">
+        /// Throws any of the exceptions that <paramref name="rightSelector"/> can throw
+        /// </exception> //// TODO TOPIC wrap this exception maybe?
+        public static IEither<TLeftValue, TRightResult> SelectRight //// TODO TOPIC name this "map" instead? //// TODO TOPIC what about the overall name?
+            <
+                TLeftValue,
+                TRightValue,
+                TRightResult,
+                TContext
+            >
+            (
+                this IEither<TLeftValue, TRightValue> either,
+                Func<TRightValue, TContext, TRightResult> rightSelector,
+                TContext context)
+        {
+            ArgumentNullException.ThrowIfNull(either);
+            ArgumentNullException.ThrowIfNull(rightSelector);
+
+            return either.Visit(
+                (left, context) => Either.Left(left).Right<TRightResult>(),
+                (right, context) => Either.Left<TLeftValue>().Right(rightSelector(right, context)),
                 context);
         }
 
