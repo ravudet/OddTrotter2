f3098e8480d712fe458b1b35148f1a2371efa688
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-07 08:20:54 -0800

    asdf

--- OddTrotter.Core.Tests/CalendarV2/Fx/Either/Either{TLeft, TRight}UnitTests.cs
+++ OddTrotter.Core.Tests/CalendarV2/Fx/Either/Either{TLeft, TRight}UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Fx.Either
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
 
     [TestClass]
     public sealed class EitherUnitTests
@@ -8,6 +9,65 @@
         [TestMethod]
         public void DoesCatchCausePerfImpact()
         {
+            //// TODO TOPIC the catch appears to run slightly faster...
+
+            var iterations = 1000000;
+
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < iterations; ++i)
+            {
+                WithoutCatch();
+            }
+
+            Console.WriteLine(stopwatch.ElapsedTicks);
+
+            stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < iterations; ++i)
+            {
+                WithCatch();
+            }
+
+            Console.WriteLine(stopwatch.ElapsedTicks);
+
+            stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < iterations; ++i)
+            {
+                WithoutCatch();
+            }
+
+            Console.WriteLine(stopwatch.ElapsedTicks);
+
+            stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < iterations; ++i)
+            {
+                WithCatch();
+            }
+
+            Console.WriteLine(stopwatch.ElapsedTicks);
+        }
+
+        private static void WithCatch()
+        {
+            try
+            {
+                DoWork();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+                throw;
+            }
+        }
+
+        private static void WithoutCatch()
+        {
+            DoWork();
+        }
+
+        private static int DoWork()
+        {
+            var random = new Random();
+            return random.Next() + 1;
         }
     }
 }
