e32dc87fbe7b1e0b8a07c4d1ace1fc5662252814
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-08 09:49:13 -0800

    asdf

--- OddTrotter.Core.Tests/CalendarV2/Fx/Either/RightMapExceptionUnitTests.cs
+++ OddTrotter.Core.Tests/CalendarV2/Fx/Either/RightMapExceptionUnitTests.cs
@@ -6,14 +6,14 @@ namespace Fx.Either
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
-    public sealed class LeftMapExceptionUnitTests
+    public sealed class RightMapExceptionUnitTests
     {
         [TestMethod]
         public void Default()
         {
-            var exception = new LeftMapException();
+            var exception = new RightMapException();
 
-            Assert.AreEqual($"Exception of type '{typeof(LeftMapException).Namespace}.{typeof(LeftMapException).Name}' was thrown.", exception.Message);
+            Assert.AreEqual($"Exception of type '{typeof(RightMapException).Namespace}.{typeof(RightMapException).Name}' was thrown.", exception.Message);
             Assert.IsNull(exception.InnerException);
         }
 
@@ -21,7 +21,7 @@ namespace Fx.Either
         public void Message()
         {
             var message = "this is the message";
-            var exception = new LeftMapException(message);
+            var exception = new RightMapException(message);
 
             Assert.AreEqual(message, exception.Message);
             Assert.IsNull(exception.InnerException);
@@ -32,7 +32,7 @@ namespace Fx.Either
         {
             var message = "this is a different message";
             var innerException = new InvalidOperationException();
-            var exception = new LeftMapException(message, innerException);
+            var exception = new RightMapException(message, innerException);
 
             Assert.AreEqual(message, exception.Message);
             Assert.AreEqual(innerException, exception.InnerException);
