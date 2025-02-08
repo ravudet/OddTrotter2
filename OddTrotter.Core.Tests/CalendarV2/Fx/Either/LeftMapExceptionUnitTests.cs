7f7380b69c1e5a117a89b1d75d20ba4dd482678f
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-08 09:46:14 -0800

    asdf

--- OddTrotter.Core.Tests/CalendarV2/Fx/Either/LeftMapExceptionUnitTests.cs
+++ OddTrotter.Core.Tests/CalendarV2/Fx/Either/LeftMapExceptionUnitTests.cs
@@ -2,9 +2,39 @@
 namespace Fx.Either
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
 
     [TestClass]
     public sealed class LeftMapExceptionUnitTests
     {
+        [TestMethod]
+        public void Default()
+        {
+            var exception = new LeftMapException();
+
+            Assert.AreEqual("Exception of type 'Fx.Either.LeftMapException' was thrown.", exception.Message);
+            Assert.IsNull(exception.InnerException);
+        }
+
+        [TestMethod]
+        public void Message()
+        {
+            var message = "this is the message";
+            var exception = new LeftMapException(message);
+
+            Assert.AreEqual(message, exception.Message);
+            Assert.IsNull(exception.InnerException);
+        }
+
+        [TestMethod]
+        public void MessageAndException()
+        {
+            var message = "this is a different message";
+            var innerException = new InvalidOperationException();
+            var exception = new LeftMapException(message, innerException);
+
+            Assert.AreEqual(message, exception.Message);
+            Assert.AreEqual(innerException, exception.InnerException);
+        }
     }
 }
