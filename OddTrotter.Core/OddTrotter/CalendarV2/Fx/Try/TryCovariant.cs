dd34ddd59b7408fdb05fd06381c49742da21292c
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 07:25:46 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/TryCovariant.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/TryCovariant.cs
@@ -2,6 +2,17 @@
 {
     using System.Diagnostics.CodeAnalysis;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TInput"></typeparam>
+    /// <typeparam name="TOutput"></typeparam>
+    /// <param name="input"></param>
+    /// <param name="tried"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// Despite the increased functionality of <see cref="TryCovariant{TInput, TOutput}"/> over <see cref="Try{TInput, TOutput}"/>, we will only use <see cref="TryCovariant{TInput, TOutput}"/> where covariance is significant (meaning that we will not implement covenience overloads that allow <see cref="TryCovariant{TInput, TOutput}"/>). The "try" pattern is a c# idiom and is convenient within the language syntax. Using <see cref="TryCovariant{TInput, TOutput}"/>, we will need to adapt it to the "try" pattern anyway, and existing .NET APIs all use the current idiom, which matches the <see cref="Try{TInput, TOutput}"/> delegate. If the covariance becomes a more popular pattern, we may add convenience overloads at that time.
+    /// </remarks>
     [return: MaybeNull]
     public delegate TOutput TryCovariant<in TInput, out TOutput>(TInput input, out bool tried);
 }
