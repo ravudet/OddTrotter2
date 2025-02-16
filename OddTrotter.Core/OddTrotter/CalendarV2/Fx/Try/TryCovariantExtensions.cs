955538bf2551d86b56719c57b2b38beda292aa64
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 07:29:57 -0800

    adsf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/TryCovariantExtensions.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/TryCovariantExtensions.cs
@@ -23,8 +23,18 @@
             return tried;
         }
 
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="try"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="try"/> is <see langword="null"/></exception>
         public static Try<TInput, TOutput> ToTry<TInput, TOutput>(this TryCovariant<TInput, TOutput> @try)
         {
+            ArgumentNullException.ThrowIfNull(@try);
+
             return @try.Try;
         }
     }
