eed2f77a25beff2c8600d45286ff73389e8ecd8a
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 07:18:03 -0800

    sdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/Try.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/Try.cs
@@ -1,19 +1,10 @@
 ﻿namespace Fx.Try
 {
-    using global::System;
-    using global::System.Collections.Generic;
     using global::System.Diagnostics.CodeAnalysis;
-    using global::System.Linq;
-
-    using global::Fx.Either;
-    using OddTrotter.TodoList;
 
     //// TODO my current conclusion:
-    //// have try (the current pattern) and trycovariant (a covariant "overload")
-    //// have an extension that uses the try pattern with trycovariant
-    //// have an extension that adapts trycovariant to try
-    //// use try everywhere that covariance is irrelevant; callers interested in consistently using trycovariant are welcome to call the adapter (this means, we should not create convenience overloads everywhere)
     //// TODO try to write the `notnull` enumerable extension
+    //// TODO use try everywhere that covariance is irrelevant; callers interested in consistently using trycovariant are welcome to call the adapter (this means, we should not create convenience overloads everywhere)
 
     public delegate bool Try<in TInput, TOutput>(TInput input, [MaybeNullWhen(false)] out TOutput output);
 }
