2b665339a03d12925fc58bb1cb20a32d77fd40a2
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 07:43:31 -0800

    asdf

---
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/Try/TryCovariantUnitTests.cs
@@ -0,0 +1,38 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx.Try
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class TryCovariantUnitTests
+    {
+        [TestMethod]
+        public void CovarianceConversion()
+        {
+            var data = new TryCovariant<Shape, Dog>[0];
+            Foo(data);
+        }
+
+        private class Shape
+        {
+        }
+
+        private class Rectangle : Shape
+        {
+        }
+
+        private class Animal
+        {
+        }
+
+        private class Dog : Animal
+        {
+        }
+
+        private static void Foo(IEnumerable<TryCovariant<Rectangle, Animal>> data)
+        {
+        }
+    }
+}
