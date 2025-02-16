948aa361498b4f1c45a311f94a7aa9d7b634b47f
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 07:25:59 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/Try.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/Try.cs
@@ -2,9 +2,6 @@
 {
     using global::System.Diagnostics.CodeAnalysis;
 
-    //// TODO my current conclusion:
-    //// TODO use try everywhere that covariance is irrelevant; callers interested in consistently using trycovariant are welcome to call the adapter (this means, we should not create convenience overloads everywhere)
-
     public delegate bool Try<in TInput, TOutput>(TInput input, [MaybeNullWhen(false)] out TOutput output);
 
     public static class Play
