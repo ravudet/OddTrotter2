c9c26a911146620e39e2b5fa1f1f02dca9d1f655
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 08:02:17 -0800

    asdf

---
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/Try/TryCovariantExtensionsUnitTests.cs
@@ -0,0 +1,85 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx.Try
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    [TestClass]
+    public sealed class TryCovariantExtensionsUnitTests
+    {
+        [TestMethod]
+        public void TryNullTry()
+        {
+            TryCovariant<string, int> @try =
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                null
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+#pragma warning disable CS8604 // Possible null reference argument.
+                @try
+#pragma warning restore CS8604 // Possible null reference argument.
+                .Try("sadf", out var input));
+        }
+
+        [TestMethod]
+        public void Try()
+        {
+            Assert.IsTrue(GetTry().Try(4, out var output));
+            Assert.AreEqual("4", output);
+
+            Assert.IsFalse(GetTry().Try(5, out output));
+            Assert.IsNull(output);
+        }
+
+        private static TryCovariant<int, string> GetTry()
+        {
+            return MakeString;
+        }
+
+        [return: MaybeNull]
+        private static string MakeString(int input, out bool success)
+        {
+            if (input % 2 == 0)
+            {
+                success = true;
+                return input.ToString();
+            }
+            else
+            {
+                success = false;
+                return null;
+            }
+        }
+
+        [TestMethod]
+        public void ToTryNullTry()
+        {
+            TryCovariant<string, int> @try =
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                null
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+#pragma warning disable CS8604 // Possible null reference argument.
+                @try
+#pragma warning restore CS8604 // Possible null reference argument.
+                .ToTry());
+        }
+
+        [TestMethod]
+        public void ToTry()
+        {
+            var @try = GetTry().ToTry();
+
+            Assert.IsTrue(@try(4, out var output));
+            Assert.AreEqual("4", output);
+
+            Assert.IsFalse(@try(5, out output));
+            Assert.IsNull(output);
+        }
+    }
+}
