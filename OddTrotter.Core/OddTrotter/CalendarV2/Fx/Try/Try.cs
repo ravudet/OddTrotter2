2c7c6dcd114080fb0775cd7cd2531791113ad1e1
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-16 07:23:01 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/Try.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/Try/Try.cs
@@ -3,8 +3,38 @@
     using global::System.Diagnostics.CodeAnalysis;
 
     //// TODO my current conclusion:
-    //// TODO try to write the `notnull` enumerable extension
     //// TODO use try everywhere that covariance is irrelevant; callers interested in consistently using trycovariant are welcome to call the adapter (this means, we should not create convenience overloads everywhere)
 
     public delegate bool Try<in TInput, TOutput>(TInput input, [MaybeNullWhen(false)] out TOutput output);
+
+    public static class Play
+    {
+        public static System.Collections.Generic.IEnumerable<T> NotNull<T>(this System.Collections.Generic.IEnumerable<T?> source)
+        {
+            return source.TrySelect<T?, T>(TryGetValue);
+        }
+
+        public static bool TryGetValue<T>(this T? nullable, [MaybeNullWhen(false)] out T value)
+        {
+            if (nullable == null)
+            {
+                value = default;
+                return false;
+            }
+
+            value = nullable;
+            return true;
+        }
+
+        public static System.Collections.Generic.IEnumerable<TResult> TrySelect<TSource, TResult>(this System.Collections.Generic.IEnumerable<TSource> source, Try<TSource, TResult> @try)
+        {
+            foreach (var element in source)
+            {
+                if (@try(element, out var result))
+                {
+                    yield return result;
+                }
+            }
+        }
+    }
 }
