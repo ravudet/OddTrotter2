197e9fcd6af416c915a7c9a7fbfba17c7dff1f2a
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-19 13:01:07 -0800

    sadf

---
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Linq/EnumerableExtensionsUnitTests.cs
@@ -0,0 +1,94 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace System.Linq
+{
+    using Fx.Either;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public sealed class EnumerableExtensionsUnitTests
+    {
+        [TestMethod]
+        public void EitherFirstOrDefaultNullSource()
+        {
+            IEnumerable<string> source =
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                null
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+#pragma warning disable CS8604 // Possible null reference argument.
+                source
+#pragma warning restore CS8604 // Possible null reference argument.
+                .EitherFirstOrDefault());
+        }
+
+        [TestMethod]
+        public void EitherFirstOrDefaultEmptySource()
+        {
+            var source = Enumerable.Empty<string>();
+
+            var firstOrDefault = source.EitherFirstOrDefault();
+
+            Assert.IsFalse(firstOrDefault.TryGetLeft(out var first));
+            Assert.IsTrue(firstOrDefault.TryGetRight(out var right));
+            Assert.IsNull(right);
+        }
+
+        [TestMethod]
+        public void EitherFirstOrDefaultSingleElement()
+        {
+            var element = "asfd";
+            var source = new[] { element };
+
+            var firstOrDefault = source.EitherFirstOrDefault();
+
+            Assert.IsTrue(firstOrDefault.TryGetLeft(out var left));
+            Assert.AreEqual(element, left);
+            Assert.IsFalse(firstOrDefault.TryGetRight(out var right));
+        }
+
+        [TestMethod]
+        public void EitherFirstOrDefaultWithDefaultNullSource()
+        {
+            IEnumerable<string> source =
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                null
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+#pragma warning disable CS8604 // Possible null reference argument.
+                source
+#pragma warning restore CS8604 // Possible null reference argument.
+                .EitherFirstOrDefault(42));
+        }
+
+        [TestMethod]
+        public void EitherFirstOrDefaultWithDefaultEmptySource()
+        {
+            var @default = 42;
+            var source = Enumerable.Empty<string>();
+
+            var firstOrDefault = source.EitherFirstOrDefault(@default);
+
+            Assert.IsFalse(firstOrDefault.TryGetLeft(out var left));
+            Assert.IsTrue(firstOrDefault.TryGetRight(out var right));
+            Assert.AreEqual(@default, right);
+        }
+
+        [TestMethod]
+        public void EitherFirstOrDefaultWithSingleElement()
+        {
+            var element = "Asfd";
+            var source = new[] { element };
+
+            var firstOrDefault = source.EitherFirstOrDefault(42);
+
+            Assert.IsTrue(firstOrDefault.TryGetLeft(out var left));
+            Assert.AreEqual(element, left);
+            Assert.IsFalse(firstOrDefault.TryGetRight(out var right));
+        }
+    }
+}
--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -381,11 +381,10 @@
             // unit tests
             // do everything above for unit tests
 
-            //// TODO add obsolete attribute to the implicit operator on either so you get a build warning if you actually use it; is there a way to make *this specific one* a warning instead of an error?
-            //// TODO code review firstordefaultunittests
             //// TODO then write enumerable extensions unit tests
             //// TODO then code review enumerable extensions unit tests
             //// TODO then move everything to use the new enumerable extensions
+            //// TODO add obsolete attribute to the implicit operator on either so you get a build warning if you actually use it; is there a way to make *this specific one* a warning instead of an error?
             //// TODO then do querycontext (and move everything to use it); the query context stuff will need some diligence and effort around adding the third type parameter that allows mixins to work correctly; do query contexts next probably and just know that you're going to need to put the effort in
             //// TODO do a pass cleaning up the existing code using the code quality above and either addressing todos or marking them TODO FUTURE and TODO TOPIC
 
