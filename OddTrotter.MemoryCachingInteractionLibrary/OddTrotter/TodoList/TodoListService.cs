a1cd94c1a79fac03c4dc3f8b1b5316aa7b392398
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-19 13:11:37 -0800

    asfd

--- OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
+++ OddTrotter.MemoryCachingInteractionLibrary/OddTrotter/TodoList/TodoListService.cs
@@ -381,7 +381,6 @@
             // unit tests
             // do everything above for unit tests
 
-            //// TODO then code review enumerable extensions unit tests
             //// TODO then move everything to use the new enumerable extensions
             //// TODO add obsolete attribute to the implicit operator on either so you get a build warning if you actually use it; is there a way to make *this specific one* a warning instead of an error?
             //// TODO then do querycontext (and move everything to use it); the query context stuff will need some diligence and effort around adding the third type parameter that allows mixins to work correctly; do query contexts next probably and just know that you're going to need to put the effort in
