042c47634c1593bd98ee98122db40bc1a527764d
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-21 09:11:18 -0800

    sadf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/IQueryContext.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/IQueryContext.cs
@@ -8,8 +8,12 @@
     /// </summary>
     /// <typeparam name="TValue"></typeparam>
     /// <typeparam name="TError"></typeparam>
-    public interface IQueryContext<TValue, TError>
+    public interface IQueryContext<TResponse, TValue, TError>
     {
-        Task<IQueryResult<TValue, TError>> Evaluate();
+        Task<IQueryResult<TResponse, TError>> Evaluate();
+    }
+
+    public interface IQueryContext<TValue, TError> : IQueryContext<TValue, TValue, TError> //// TODO is this overload really helpful? maybe, but do you want to set a precedent that you need to do that for the mixins too? or maybe mixins are "advanced" enough that you should expect the dev to be able to handle it
+    {
     }
 }
---
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContextOption1/Mixins/IWhereQueryContextMixin.cs
@@ -0,0 +1,10 @@
+﻿namespace Fx.QueryContextOption1.Mixins
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public interface IWhereQueryContextMixin<TResponse, TValue, TError, TQueryContext> : IQueryContext<TResponse, TValue, TError> where TQueryContext : IQueryContext<TResponse, TValue, TError>
+    {
+        TQueryContext Where(Expression<Func<TValue, bool>> predicate);
+    }
+}
