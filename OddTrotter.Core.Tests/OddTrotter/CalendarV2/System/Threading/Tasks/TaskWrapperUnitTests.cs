6262eea1e6eec318fbb3430f4f8c832fd8e8d424
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-22 14:02:23 -0800

    asdf

---
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/System/Threading/Tasks/TaskWrapperUnitTests.cs
@@ -0,0 +1,35 @@
+﻿namespace System.Threading.Tasks
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class TaskWrapperUnitTests
+    {
+        [TestMethod]
+        public async Task Await()
+        {
+            var value = await new AwaitedType().GetValue2();
+            Assert.AreEqual("asdfasdf3asdf2", value);
+        }
+
+        private sealed class AwaitedType
+        {
+            public ITask<string> GetValue()
+            {
+                return new TaskWrapper<string>(Task.Delay(100).ContinueWith(_ => "asdf"));
+            }
+
+            public ITask<string> GetAnotherValue()
+            {
+                return new TaskWrapper<string>(Task.FromResult("asdf3"));
+            }
+
+            public async Task<string> GetValue2()
+            {
+                var value = await GetValue();
+                var value2 = await GetAnotherValue();
+                return value + value2 + "asdf2";
+            }
+        }
+    }
+}
