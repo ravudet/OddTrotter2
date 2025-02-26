a8bdb58febe73c2fc1f9751b2e0a5bb68f5be5f7
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-26 14:50:06 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/IQueryResultNode.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/IQueryResultNode.cs
@@ -1,10 +1,10 @@
-﻿namespace Fx.QueryContext
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx.QueryContext
 {
-    using System;
-
     using Fx.Either;
 
-    public interface IQueryResultNode<out TValue, out TError> : IEither<IElement<TValue, TError>, IEither<IError<TError>, IEmpty>>
+    public interface IQueryResultNode<out TValue, out TError> 
+        : IEither<IElement<TValue, TError>, IEither<IError<TError>, IEmpty>>
     {
     }
 }
