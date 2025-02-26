dbc7e4a073ac095f18438937a6705c17ec0553d7
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-26 14:53:34 -0800

    asfd

--- OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultNode.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultNode.cs
@@ -1,19 +1,36 @@
-﻿namespace Fx.QueryContext
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx.QueryContext
 {
+    using System;
+
     using Fx.Either;
 
     public sealed class QueryResultNode<TValue, TError> : IQueryResultNode<TValue, TError>
     {
         private readonly IEither<IElement<TValue, TError>, IEither<IError<TError>, IEmpty>> node;
 
+        /// <summary>
+        /// placeholder
+        /// </summary>
+        /// <param name="node"></param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="node"/> is <see langword="null"/></exception>
         public QueryResultNode(IEither<IElement<TValue, TError>, IEither<IError<TError>, IEmpty>> node)
         {
+            ArgumentNullException.ThrowIfNull(node);
+
             this.node = node;
         }
 
-        public TResult Apply<TResult, TContext>(System.Func<IElement<TValue, TError>, TContext, TResult> leftMap, System.Func<IEither<IError<TError>, IEmpty>, TContext, TResult> rightMap, TContext context)
+        /// <inheritdoc/>
+        public TResult Apply<TResult, TContext>(
+            Func<IElement<TValue, TError>, TContext, TResult> leftMap, 
+            Func<IEither<IError<TError>, IEmpty>, TContext, TResult> rightMap, 
+            TContext context)
         {
-            return node.Apply(leftMap, rightMap, context);
+            ArgumentNullException.ThrowIfNull(leftMap);
+            ArgumentNullException.ThrowIfNull(rightMap);
+
+            return this.node.Apply(leftMap, rightMap, context);
         }
     }
 }
