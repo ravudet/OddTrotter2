a16b25ed41321e8b9530edceb19175f07786366e
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-26 16:54:27 -0800

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensionsUnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensionsUnitTests.cs
@@ -46,5 +46,110 @@
 #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
                 ));
         }
+        [TestMethod]
+        public void WhereNoElements()
+        {
+            var queryResult = new MockQueryResult(Either.Left<MockElement>().Right(Either.Left<MockError>().Right(MockEmpty.Instance)).ToQueryResultNode());
+
+            var whered = queryResult.Where(_ => true);
+
+            Assert.IsFalse(whered.Nodes.TryGetLeft(out var element));
+            Assert.IsTrue(whered.Nodes.TryGetRight(out var terminal));
+            Assert.IsFalse(terminal.TryGetLeft(out var error));
+            Assert.IsTrue(terminal.TryGetRight(out var empty));
+        }
+
+        [TestMethod]
+        public void WhereNoElementsError()
+        {
+            var invalidOperationException = new InvalidOperationException();
+            var queryResult = 
+                new MockQueryResult(
+                    Either
+                        .Left<MockElement>()
+                        .Right(
+                            Either
+                                .Left(
+                                    new MockError(invalidOperationException))
+                                .Right<MockEmpty>())
+                        .ToQueryResultNode());
+
+            var whered = queryResult.Where(_ => true);
+
+            Assert.IsFalse(whered.Nodes.TryGetLeft(out var element));
+            Assert.IsTrue(whered.Nodes.TryGetRight(out var terminal));
+            Assert.IsTrue(terminal.TryGetLeft(out var error));
+            Assert.AreEqual(invalidOperationException, error.Value);
+            Assert.IsFalse(terminal.TryGetRight(out var empty));
+        }
+
+        [TestMethod]
+        public void WhereNoError()
+        {
+            var value = "asdf";
+            var queryResult =
+                new MockQueryResult(Either.Left(new MockElement(value)).Right<IEither<MockError, MockEmpty>>().ToQueryResultNode());
+
+            var whered = queryResult.Where(_ => true);
+
+            Assert.IsTrue(whered.Nodes.TryGetLeft(out var element));
+            Assert.AreEqual(value, element.Value);
+            var next = element.Next();
+            Assert.IsFalse(next.TryGetLeft(out var secondElement));
+            Assert.IsTrue(next.TryGetRight(out var secondTerminal));
+            Assert.IsFalse(secondTerminal.TryGetLeft(out var secondError));
+            Assert.IsTrue(secondTerminal.TryGetRight(out var secondEmpty));
+            Assert.IsFalse(whered.Nodes.TryGetRight(out var terminal));
+
+            whered = queryResult.Where(_ => false);
+
+            Assert.IsFalse(whered.Nodes.TryGetLeft(out element));
+            Assert.IsTrue(whered.Nodes.TryGetRight(out var terminal2));
+            Assert.IsFalse(terminal2.TryGetLeft(out var error));
+            Assert.IsTrue(terminal2.TryGetRight(out var empty));
+        }
+
+        [TestMethod]
+        public void WhereElementFollowedByError()
+        {
+            var value = "asdf";
+            var invalidOperationException = new InvalidOperationException();
+            var queryResult =
+                new MockQueryResult(
+                    Either
+                        .Left(
+                            new MockElement(
+                                value,
+                                Either
+                                    .Left<MockElement>()
+                                    .Right(
+                                        Either
+                                            .Left(
+                                                new MockError(invalidOperationException))
+                                            .Right<MockEmpty>())
+                                    .ToQueryResultNode()))
+                        .Right<IEither<MockError, MockEmpty>>()
+                        .ToQueryResultNode());
+
+            var whered = queryResult.Where(_ => true);
+
+            Assert.IsTrue(whered.Nodes.TryGetLeft(out var element));
+            Assert.AreEqual(value, element.Value);
+            var next = element.Next();
+            Assert.IsFalse(next.TryGetLeft(out var secondElement));
+            Assert.IsTrue(next.TryGetRight(out var secondTerminal));
+            Assert.IsTrue(secondTerminal.TryGetLeft(out var secondError));
+            Assert.AreEqual(invalidOperationException, secondError.Value);
+            Assert.IsFalse(secondTerminal.TryGetRight(out var secondEmpty));
+            Assert.IsFalse(whered.Nodes.TryGetRight(out var terminal));
+
+            whered = queryResult.Where(_ => false);
+
+            Assert.IsFalse(whered.Nodes.TryGetLeft(out element));
+            Assert.IsTrue(whered.Nodes.TryGetRight(out var terminal2));
+            Assert.IsTrue(terminal2.TryGetLeft(out var error));
+            Assert.AreEqual(invalidOperationException, error.Value);
+            Assert.IsFalse(terminal2.TryGetRight(out var empty));
+        }
     }
 }
