cd464ec4578159fab2ff5d46923fd631724600ff
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-28 07:06:54 -0800

    asdf

--- OddTrotter.Core/OddTrotter/CalendarV2/System/Linq/FirstOrDefault.cs
+++ OddTrotter.Core/OddTrotter/CalendarV2/System/Linq/FirstOrDefault.cs
@@ -1,47 +1,12 @@
-﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-namespace System.Linq
+﻿namespace System.Linq
 {
-    using System;
-
     using Fx.Either;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="TFirst"></typeparam>
-    /// <typeparam name="TDefault"></typeparam>
-    /// <remarks>
-    /// You were tempted to nest this in <see cref="EnumerableExtensions"/>, but realized that its name would conflict with the
-    /// <see cref="EnumerableExtensions.EitherFirstOrDefault{TElement}(Collections.Generic.IEnumerable{TElement})"/> if you had
-    /// named *that* method how you wanted to, but you couldn't because then it would conflict with the existing LINQ extension.
-    /// So, we are choosing to not compound mistakes and are putting this in its own file.
-    /// </remarks>
-    public sealed class FirstOrDefault<TFirst, TDefault> : IEither<TFirst, TDefault>
+    public static class FirstOrDefault
     {
-        private readonly IEither<TFirst, TDefault> either;
-
-        /// <summary>
-        /// placeholder
-        /// </summary>
-        /// <param name="either"></param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="either"/> is <see langword="null"/></exception>
-        public FirstOrDefault(IEither<TFirst, TDefault> either)
+        public static FirstOrDefault<TFirst, TDefault> Create<TFirst, TDefault>(IEither<TFirst, TDefault> either)
         {
-            ArgumentNullException.ThrowIfNull(either);
-
-            this.either = either;
-        }
-
-        /// <inheritdoc/>
-        public TResult Apply<TResult, TContext>(
-            Func<TFirst, TContext, TResult> leftMap,
-            Func<TDefault, TContext, TResult> rightMap,
-            TContext context)
-        {
-            ArgumentNullException.ThrowIfNull(leftMap);
-            ArgumentNullException.ThrowIfNull(rightMap);
-
-            return this.either.Apply(leftMap, rightMap, context);
+            return new FirstOrDefault<TFirst, TDefault>(either);
         }
     }
 }
