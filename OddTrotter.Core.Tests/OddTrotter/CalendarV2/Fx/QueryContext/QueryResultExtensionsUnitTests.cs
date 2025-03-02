670581c98723a0bfac227c09103a1475dcac1ac2
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-02 09:24:56 -0800

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensionsUnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensionsUnitTests.cs
@@ -977,6 +977,58 @@ namespace Fx.QueryContext
             Assert.IsFalse(next.TryGetRight(out var nextTerminal));
             Assert.IsFalse(concated.Nodes.TryGetRight(out var terminal));
         }
+
+        [TestMethod]
+        public void ConcatFirstElementNoErrorSecondElementError()
+        {
+            var firstValue = "Asdf";
+            var first =
+                new MockQueryResult(
+                    Either
+                        .Left(
+                            new MockElement(
+                                    firstValue))
+                        .Right<IEither<IError<Exception>, IEmpty>>()
+                        .ToQueryResultNode());
+            var secondValue = "qwer";
+            var invalidCastException = new InvalidCastException();
+            var second =
+                new MockQueryResult(
+                    Either
+                        .Left(
+                            new MockElement(
+                                secondValue,
+                                Either
+                                    .Left<IElement<string, Exception>>()
+                                    .Right(
+                                        Either
+                                            .Left(new MockError(invalidCastException))
+                                            .Right<IEmpty>())
+                                    .ToQueryResultNode()))
+                        .Right<IEither<IError<Exception>, IEmpty>>()
+                        .ToQueryResultNode());
+
+            var concated = first.Concat(
+                second,
+                firstError => new AggregateException(firstError),
+                secondError => new AggregateException(secondError),
+                (firstError, secondError) => new AggregateException(firstError, secondError));
+
+            Assert.IsTrue(concated.Nodes.TryGetLeft(out var element));
+            Assert.AreEqual(firstValue, element.Value);
+            var next = element.Next();
+            Assert.IsTrue(next.TryGetLeft(out var nextElement));
+            Assert.AreEqual(secondValue, nextElement.Value);
+            var nextNext = nextElement.Next();
+            Assert.IsFalse(nextNext.TryGetLeft(out var nextNextElement));
+            Assert.IsTrue(nextNext.TryGetRight(out var nextNextTerminal));
+            Assert.IsTrue(nextNextTerminal.TryGetLeft(out var nextNextError));
+            Assert.AreEqual(1, nextNextError.Value.InnerExceptions.Count);
+            Assert.AreEqual(invalidCastException, nextNextError.Value.InnerExceptions[0]);
+            Assert.IsFalse(nextNextTerminal.TryGetRight(out var nextNextEmpty));
+            Assert.IsFalse(next.TryGetRight(out var nextTerminal));
+            Assert.IsFalse(concated.Nodes.TryGetRight(out var terminal));
+        }
 /*
 first element   first error     second element      second error
 1               0               1                   1
