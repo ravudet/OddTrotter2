f5e304efedaa8f8a783a64c4e08672ddb01e8193
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-03 07:38:47 -0800

    asdf

---
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/RealNullableUnitTests.cs
@@ -0,0 +1,45 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Fx
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class RealNullableUnitTests
+    {
+        [TestMethod]
+        public void DefaultInitializer()
+        {
+            var nullable = new RealNullable<int?>();
+
+            Assert.IsFalse(nullable.TryGetValue(out var value));
+        }
+
+        [TestMethod]
+        public void Default()
+        {
+            RealNullable<int?> nullable = default;
+
+            Assert.IsFalse(nullable.TryGetValue(out var value));
+        }
+
+        [TestMethod]
+        public void Value()
+        {
+            var providedValue = 42;
+            var nullable = new RealNullable<int?>(providedValue);
+
+            Assert.IsTrue(nullable.TryGetValue(out var value));
+            Assert.AreEqual(providedValue, value);
+        }
+
+        [TestMethod]
+        public void Null()
+        {
+            int? providedValue = null;
+            var nullable = new RealNullable<int?>(providedValue);
+
+            Assert.IsTrue(nullable.TryGetValue(out var value));
+            Assert.AreEqual(providedValue, value);
+        }
+    }
+}
