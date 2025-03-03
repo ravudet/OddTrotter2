73b9483ee49876b3f0cb35f524488c1b0bb7a121
ravudet <170215607+ravudet@users.noreply.github.com>
2025-03-03 08:10:58 -0800

    asdf

--- OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensionsUnitTests.cs
+++ OddTrotter.Core.Tests/OddTrotter/CalendarV2/Fx/QueryContext/QueryResultExtensionsUnitTests.cs
@@ -1306,52 +1306,6 @@ namespace Fx.QueryContext
             public IQueryResultNode<string, int?> Nodes { get; }
         }
 
-        private sealed class NullableErrorElement : IElement<string, int?>
-        {
-            private readonly IQueryResultNode<string, int?> next;
-
-            /// <summary>
-            /// placeholder
-            /// </summary>
-            /// <param name="value"></param>
-            public NullableErrorElement(string value)
-                : this(
-                    value,
-                    Either
-                        .Left<IElement<string, int?>>()
-                        .Right(
-                            Either
-                                .Left<IError<int?>>()
-                                .Right(
-                                    MockEmpty.Instance))
-                        .ToQueryResultNode())
-            {
-            }
-
-            /// <summary>
-            /// placeholder
-            /// </summary>
-            /// <param name="value"></param>
-            /// <param name="next"></param>
-            /// <exception cref="ArgumentNullException">Thrown if <paramref name="next"/> is <see langword="null"/></exception>
-            public NullableErrorElement(string value, IQueryResultNode<string, int?> next)
-            {
-                ArgumentNullException.ThrowIfNull(next);
-
-                this.Value = value;
-                this.next = next;
-            }
-
-            /// <inheritdoc/>
-            public string Value { get; }
-
-            /// <inheritdoc/>
-            public IQueryResultNode<string, int?> Next()
-            {
-                return this.next;
-            }
-        }
-
         [TestMethod]
         public void ConcatNullableErrorNullFirstErrorSecondError()
         {
